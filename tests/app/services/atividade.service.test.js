89b27f8b7030770101c08ddfb131dc689054c962
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2020-11-26 20:20:30 -0300

    Adiciona teste de serviço de matricula

---
+++ tests/app/services/atividade.service.test.js
@@ -0,0 +1,7 @@
+const { Disciplina } = require('../../../src/app/models/disciplina');
+const { Usuario } = require('../../../src/app/models/usuario');
+const { Service } = require('../../../src/app/services/disciplina.service');
+
+
+
+
