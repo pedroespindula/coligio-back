4c31b09805343796c51db7ea99a8c090b947f89d
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2020-11-26 20:22:35 -0300

    Conserta erro em serviço de matricula

--- tests/app/services/matricula.service.test.js
+++ tests/app/services/matricula.service.test.js
@@ -1,12 +1,12 @@
 const { Disciplina } = require('../../../src/app/models/disciplina');
 const { Usuario } = require('../../../src/app/models/usuario');
 const { Matricula } = require('../../../src/app/models/matricula');
-const { Service } = require('../../../src/app/services/disciplina.service');
+const { Service } = require('../../../src/app/services/matricula.service');
 
 
 test("matricula aluno", () => {
     expect(Service.matriculaAluno(1434134,12314)).toBe(Error);
-    const d1 = await Service.create({nome:"engenharia de software", semestre:"RAE", carga:60}, 4);
+    const d1 = await Disciplina.create({nome:"engenharia de software", semestre:"RAE", carga:60}, 4);
     expect(Service.matriculaAluno(13657,1)).toBe(Error);
     const u1 = await Usuario.create({nome:"Vitor", senha:"1234", email:"vitor@gmail.com", cargo: "aluno"});
     expect(Service.matriculaAluno(1,1)).toBe(await Matricula.create({usuarioId:1, disciplinaId:1}));
@@ -15,10 +15,11 @@ test("matricula aluno", () => {
 
 test("desmatricula aluno", () => {
     expect(Service.desmatriculaAluno(1,1)).toBe(null);
-    const d1 = await Service.create({nome:"engenharia de software", semestre:"RAE", carga:60}, 4);
+    const d1 = await Disciplina.create({nome:"engenharia de software", semestre:"RAE", carga:60}, 4);
     expect(Service.desmatriculaAluno(1,1)).toBe(null);
     const u1 = await Usuario.create({nome:"Vitor", senha:"1234", email:"vitor@gmail.com", cargo: "aluno"});
     const m1 = await Matricula.create({usuarioId:1, disciplinaId:1});
     expect(Service.desmatriculaAluno(1,1)).toBe(m1);
+    expect(Service.desmatriculaAluno(1,1)).toBe(null);
 });
 
