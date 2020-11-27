acc8a20856f9d9b3e4477d61710455253b02ab69
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2020-11-27 20:29:38 -0300

    Add matricula.service.test.js

--- tests/app/services/matricula.service.test.js
+++ tests/app/services/matricula.service.test.js
@@ -2,24 +2,30 @@ const { Disciplina } = require('../../../src/app/models/disciplina');
 const { Usuario } = require('../../../src/app/models/usuario');
 const { Matricula } = require('../../../src/app/models/matricula');
 const Service = require('../../../src/app/services/matricula.service');
+const DisciplinaService = require('../../../src/app/services/disciplina.service');
+const AlunoService = require('../../../src/app/services/usuario.service');
 
+let aluno;
+const criaAluno = async () => {
+    aluno = await AlunoService.create({nome: "Vitor", senha: "12345678", email: "vitor@gmail.com", cargo: "professor"});
+
+    if (!aluno) {
+        alunos = await AlunoService.get();
+        aluno = alunos[0];
+    }
+}
 
 test("matricula aluno", async () => {
-    expect(await Service.matricular(1434134,12314)).toThrow(Error);
-    const d1 = await Disciplina.create({nome:"engenharia de software", semestre:"RAE", carga:60}, 4);
-    expect(await Service.matricular(13657,1)).toThrow(Error);
-    const u1 = await Usuario.create({nome:"Vitor", senha:"1234", email:"vitor@gmail.com", cargo: "aluno"});
-    expect(await Service.matricular(1,1)).toContain({usuarioId:1, disciplinaId:1});
-    expect(await Service.matricular(1,1)).toBeNull();
+    await criaAluno();
+    await DisciplinaService.deleteByName('engenharia de software');
+    const d = await DisciplinaService.create({nome:"engenharia de software", semestre:"RAE", cargaHoraria:60}, aluno.id);
+    expect(await Service.matricular(d.id,aluno.id)).not.toBeNull();
 });
 
 test("desmatricula aluno", async () => {
-    expect(await Service.desmatricular(1,1)).toBeNull();
-    const d1 = await Disciplina.create({nome:"engenharia de software", semestre:"RAE", carga:60}, 4);
-    expect(await Service.desmatricular(1,1)).toBeNull();
-    const u1 = await Usuario.create({nome:"Vitor", senha:"1234", email:"vitor@gmail.com", cargo: "aluno"});
-    const m1 = await Matricula.create({usuarioId:1, disciplinaId:1});
-    expect(await Service.desmatricular(1,1)).toContain({usuarioId:1, disciplinaId:1});
-    expect(await Service.desmatricular(1,1)).toBeNull();
+    await DisciplinaService.deleteByName('engenharia de software 2');
+    const d = await DisciplinaService.create({nome:"engenharia de software 2", semestre:"RAE", cargaHoraria:60}, aluno.id);
+    await Service.matricular(d.id,aluno.id)
+    expect(await Service.desmatricular(d.id,aluno.id)).not.toBeNull();
 });
 
