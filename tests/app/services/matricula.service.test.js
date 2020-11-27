2638c64ba0cf7ca3aee55438eafaf9765befd414
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2020-11-27 17:50:27 -0300

    Conserta bugs de testes

--- tests/app/services/matricula.service.test.js
+++ tests/app/services/matricula.service.test.js
@@ -4,22 +4,22 @@ const { Matricula } = require('../../../src/app/models/matricula');
 const { Service } = require('../../../src/app/services/matricula.service');
 
 
-test("matricula aluno", () => {
-    expect(Service.matriculaAluno(1434134,12314)).toThrow(Error);
+test("matricula aluno", async () => {
+    expect(await Service.matriculaAluno(1434134,12314)).toThrow(Error);
     const d1 = await Disciplina.create({nome:"engenharia de software", semestre:"RAE", carga:60}, 4);
-    expect(Service.matriculaAluno(13657,1)).toThrow(Error);
+    expect(await Service.matriculaAluno(13657,1)).toThrow(Error);
     const u1 = await Usuario.create({nome:"Vitor", senha:"1234", email:"vitor@gmail.com", cargo: "aluno"});
-    expect(Service.matriculaAluno(1,1)).toContain({usuarioId:1, disciplinaId:1});
-    expect(Service.matriculaAluno(1,1)).toBeNull();
+    expect(await Service.matriculaAluno(1,1)).toContain({usuarioId:1, disciplinaId:1});
+    expect(await Service.matriculaAluno(1,1)).toBeNull();
 });
 
-test("desmatricula aluno", () => {
-    expect(Service.desmatriculaAluno(1,1)).toBeNull();
+test("desmatricula aluno", async () => {
+    expect(await Service.desmatriculaAluno(1,1)).toBeNull();
     const d1 = await Disciplina.create({nome:"engenharia de software", semestre:"RAE", carga:60}, 4);
-    expect(Service.desmatriculaAluno(1,1)).toBeNull();
+    expect(await Service.desmatriculaAluno(1,1)).toBeNull();
     const u1 = await Usuario.create({nome:"Vitor", senha:"1234", email:"vitor@gmail.com", cargo: "aluno"});
     const m1 = await Matricula.create({usuarioId:1, disciplinaId:1});
-    expect(Service.desmatriculaAluno(1,1)).toContain({usuarioId:1, disciplinaId:1});
-    expect(Service.desmatriculaAluno(1,1)).toBeNull();
+    expect(await Service.desmatriculaAluno(1,1)).toContain({usuarioId:1, disciplinaId:1});
+    expect(await Service.desmatriculaAluno(1,1)).toBeNull();
 });
 
--- tests/app/services/usuario.service.test.js
+++ tests/app/services/usuario.service.test.js
@@ -3,53 +3,53 @@ const { Service } = require('../../../src/app/services/usuario.service');
 
 
 //Teste de criação de um novo usuário
-test("cria novo usuario", () =>{
-    expect(Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno")).toContain({nome:"Vitor", senha:"12345678", email:"vitor@gmail.com", cargo:"aluno"});
-    expect(Service.create("Victor", "87654321", "victor@gmail.com", "professor")).toBe({nome:"Victor", email:"victor@gmail.com"});
-    expect(Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno")).toBeNull();
-    expect(Service.create("Victor", "87654321", "victor@gmail.com", "professor")).toBeNull();
+test("cria novo usuario", async() =>{
+    expect(await Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno")).toContain({nome:"Vitor", senha:"12345678", email:"vitor@gmail.com", cargo:"aluno"});
+    expect(await Service.create("Victor", "87654321", "victor@gmail.com", "professor")).toContain({nome:"Victor", email:"victor@gmail.com"});
+    expect(await Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno")).toBeNull();
+    expect(await Service.create("Victor", "87654321", "victor@gmail.com", "professor")).toBeNull();
 });
 
 //Teste de busca de usuários cadastrados
-test("busca todos os usuarios", () => {
-    expect(Service.get()).toHaveLength(0);
+test("busca todos os usuarios", async() => {
+    expect(await Service.get()).toHaveLength(0);
 
-    const u1 = Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno");
-    expect(Service.get()).toHaveLength(1);
+    const u1 = await Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno");
+    expect(await Service.get()).toHaveLength(1);
 
-    const u2 = Service.create("Victor", "87654321", "victor@gmail.com", "professor");
-    expect(Service.get()).toHaveLength(2);
+    const u2 = await Service.create("Victor", "87654321", "victor@gmail.com", "professor");
+    expect(await Service.get()).toHaveLength(2);
 });
 
 //Teste de busca por um usuário específico
-test("busca usuario por id", () =>{
-    expect(Service.getById(1)).toBeNull();
-    const u1 = Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno");
-    expect(Service.get(1)).toContain(u1);
+test("busca usuario por id", async() =>{
+    expect(await Service.getById(1)).toBeNull();
+    const u1 = await Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno");
+    expect(await Service.get(1)).toContain(u1);
 
     const u2 = Service.create("Victor", "87654321", "victor@gmail.com", "professor");
-    expect(Service.get(2)).toContain(u2);
+    expect(await Service.get(2)).toContain(u2);
 
-    expect(Service.get(3)).toBeNull();
+    expect(await Service.get(3)).toBeNull();
 });
 
 //Teste de edição de usuário
-test("edita usuario", () => {
+test("edita usuario", async() => {
     var u1 = Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno");
-    expect(Service.edit(1, {nome: "Severino"})).toContain({nome: "Severino"});
-    expect(Service.edit(1, {nome: "Jose"})).toContain({nome: "Jose"});
-    expect(Service.edit(1, {nome: "Adroaldo"})).toContain({nome: "Adroaldo"});
+    expect(await Service.edit(1, {nome: "Severino"})).toContain({nome: "Severino"});
+    expect(await Service.edit(1, {nome: "Jose"})).toContain({nome: "Jose"});
+    expect(await Service.edit(1, {nome: "Adroaldo"})).toContain({nome: "Adroaldo"});
 });
 
 //Teste de deleção de usuário
-test("deleta usuario", () =>{
-    const u1 = Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno");
-    expect(Service.deleteById(1)).toContain(u1);
+test("deleta usuario", async() =>{
+    const u1 = await Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno");
+    expect(await Service.deleteById(1)).toContain(u1);
 });
 
 
 //Teste de login
-test("loga usuario", () =>{
+test("loga usuario", async() =>{
     const u1 = Service.create("Vitor", "12345678", "vitor@gmail.com", "aluno");
-    expect(Service.login("vitor@gmail.com", "12345678")).toContain({u1});
+    expect(await Service.login("vitor@gmail.com", "12345678")).toContain({u1});
 });
